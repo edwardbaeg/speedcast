f7aeb4cc8dd55022379f7a1ed8c17ebf636d3bb5
edwardbaeg <edwardbaeg9@gmail.com>
2018-08-26 13:26:31 -0700

    Create database seeding script

---
+++ database-mongo/seeder.js
@@ -0,0 +1,23 @@
+const db = require('./index.js');
+
+const clearData = () => {
+  db.Podcast.remove({}, err => {
+    if (err) {
+      console.log('db error', err);
+    } else {
+      console.log('database cleared');
+    }
+  });
+};
+
+const pods = [
+  {
+    url: 'https://content.production.cdn.art19.com/episodes/e379802b-e812-4148-bafc-ef338379d0f7/1eff18a0c2eb8828ce9865b63fa6edfda6381b4c0cff924745e7c0c622e7046cd7c5ea23fc980e54f93cd0680ef67bdcae761acd4cb7c39dc2285ffd38d4db70/PC%20HAPPINESS%20MIX%20GR%20180815%20R1.mp3<Paste>',
+    title: 'How To Be Happy',
+    channel: 'Freakonomics',
+  },
+];
+
+db.Podcast.insertMany(pods)
+  .then(() => console.log('db seeded'))
+  .catch(err => console.log('seeding error'));
